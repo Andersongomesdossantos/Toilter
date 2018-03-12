dfec3c9c79be3aecc810becc911245fc14f02029
Andersongomesdossantos <djandersongomesdnb@gmail.com>
2018-03-12 20:48:32 -0300

    Commit inicial

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object('config')
-
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/devel/Documentos/python/toilter/storage.db'
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-#from app.models import tables
+
 from app.controllers import default
+#from app.models import default
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,7 @@
-from flask import render_template, url_for
 from app import app
-from app.models.forms import LoginForm
 
 
-@app.route("/index")
+
 @app.route("/")
 def index():
-    return render_template('index.html')
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
-    return render_template('login.html', form=form)
+    return "Hello World!!"
